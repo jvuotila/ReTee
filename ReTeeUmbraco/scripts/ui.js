502bbdb5531f113543c694d2301d37caaf11b8f6
Juho Vuotila <juho.vuotila@gmail.com>
2015-10-11 19:22:09 +0300

    Umbraco CMS with temporary home page

---
+++ ReTeeUmbraco/scripts/ui.js
@@ -0,0 +1,14 @@
+﻿function changeLang() {
+    switch($("html").prop("lang").toLowerCase()) {
+        case "en-us":
+            location.replace("http://" + window.location.host);
+            break;
+        case "fi-fi":
+            location.replace("http://" + window.location.host + "/en");
+            break;
+        default:
+            return false;
+    }
+
+    return false;
+}
